5a36532699159af0caeee9aca9ee1ed2a1ad88ff
Saúl Caballero <saul.caballero.ramirez@gmail.com>
2019-08-21 13:36:44 -0500

    Agrega simulaciones de Markov via gsl

--- r_code/try.R
+++ r_code/try.R
@@ -1,24 +1,7 @@
-run.mc.sim <- function( P, num.iters = 50 ) {
-
-  # number of possible states
-  num.states <- nrow(P)
-
-  # stores the states X_t through time
-  states     <- numeric(num.iters)
-  print(states)
-  # initialize variable for first state
-  states[1]    <- 1
-  # print(states)
-  for(t in 2:num.iters) {
-
-    # probability vector to simulate next state X_{t+1}
-    p  <- P[states[t-1], ]
-    states[t] <-  which(rmultinom(1, 1, p) == 1)
-  }
-  return(states)
-}
+library(Rcpp)
+library(RcppArmadillo)
+library(RcppGSL)
 
 Rcpp::sourceCpp("rcpp/simulator.cpp")
-a = create_matrix(10000)
-run.mc.sim(create_matrix(50))
-dim(a)
+a = create_matrix(100)
+sim_mc(10000, a, 1)
--- rcpp/simulator.cpp
+++ rcpp/simulator.cpp
@@ -1,6 +1,12 @@
-#include <ostream>
-#include <RcppArmadillo.h>
 // [[Rcpp::depends(RcppArmadillo)]]
+// [[Rcpp::depends(RcppGSL)]]
+#include <RcppArmadillo.h>
+#include <RcppGSL.h>
+#include <gsl/gsl_rng.h>
+#include <gsl/gsl_randist.h>
+#include <ostream>
+#include <iostream>
+using namespace std;
 
 // [[Rcpp::export]]
 arma::mat create_matrix (int n){
@@ -13,4 +19,34 @@ arma::mat create_matrix (int n){
     }
   }
   return(result);
+}
+
+int multinom(arma::mat trans_matrix, int state, const gsl_rng * r, int K){
+  double prob[K];
+  for(int i = 0; i < K; i++){
+    prob[i] = trans_matrix(state - 1, i);
+  }
+  Rcpp::IntegerVector x(K);
+  gsl_ran_multinomial(r, K, 1, prob, (unsigned int *) x.begin());
+  int val;
+  for(int i = 0; i < K; i++){
+    if(x(i) == 1){
+      val = i + 1;
+    }
+  }
+  return(val);
+}
+
+// [[Rcpp::export]]
+Rcpp::IntegerVector sim_mc(int P, arma::mat transition_matrix, int seed){
+  int states = transition_matrix.n_cols;
+  Rcpp::IntegerVector sim(P);
+  gsl_rng * rand_gen = gsl_rng_alloc(gsl_rng_taus);
+  gsl_rng_set(rand_gen, seed);
+  sim(0) = 1;
+  for(int i = 1; i < P; i++){
+    sim(i) = multinom(transition_matrix, sim(i - 1), rand_gen, states);
+  }
+  gsl_rng_free(rand_gen);
+  return(sim);
 }
